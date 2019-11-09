ecc8b419d8c56c23f33d356b9cc3e8fce698708e
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2019-11-09 10:37:09 +0000

    plotting single variable as svg using reportlab

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -2,14 +2,19 @@ import unittest
 from weather_reporter import Layout
 import os
 
-path = os.path.join(os.path.dirname(__file__), 'sample_data/sample.txt')
+tests = os.path.dirname(__file__)
+sample_data = os.path.join(tests, 'sample_data/sample.txt')
+outputs = os.path.join(tests, 'outputs')
+if not os.path.exists(outputs):
+    os.mkdir(outputs)
 
 
 class TestLayout(unittest.TestCase):
 
-    def test_plot_all_variables(self):
-        layout = Layout(path)
-        layout.create_pdf('all_plots.pdf')
+    def test_temperature(self):
+        layout = Layout(sample_data)
+        layout.set_variable('temp_out')
+        layout.create_pdf(os.path.join(outputs, 'temp_out.pdf'))
 
 
 
--- weather_reporter/layout.py
+++ weather_reporter/layout.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-from jinja2 import Template
-from xhtml2pdf import pisa
-
+from reportlab.platypus import SimpleDocTemplate
+from svglib.svglib import svg2rlg
+from reportlab.lib.pagesizes import A4, landscape
 image_dir = os.path.join(os.path.dirname(__file__), '.images')
 if not os.path.exists(image_dir):
     os.mkdir(image_dir)
@@ -13,23 +13,21 @@ class Layout:
     def __init__(self, path):
         self.path = path
         self.df: pd.DataFrame = None
-        self.numeric_variables: list = None
+        self.variables: list = None
         self.start_date: pd.datetime = None
         self.end_date: pd.datetime = None
-        self.plots: list = None
+        self.plot: str = None
+        self.variable = None
 
         self.read_dataset()
-        self.plot_variables()
+        self.save_plot()
 
-    def plot_variables(self):
-        self.plots = []
-        for variable in self.numeric_variables:
-            f, ax = plt.subplots(figsize=(8, 3), dpi=300)
-            self.df[self.start_date:self.end_date][variable].plot(ax=ax, title=variable)
-            plt.tight_layout()
-            path = os.path.join(image_dir, '{}.png'.format(variable))
-            f.savefig(path)
-            self.plots.append(path)
+    def save_plot(self):
+        f, ax = plt.subplots(figsize=(9, 6))
+        self.df[self.start_date:self.end_date][self.variable].plot(ax=ax, title=self.variable)
+        plt.tight_layout()
+        self.plot = os.path.join(image_dir, '{}.svg'.format(self.variable))
+        f.savefig(self.plot)
 
     def read_dataset(self):
         self.df = pd.read_csv(self.path, sep='\t', parse_dates=[[0, 1]], header=[0,1])
@@ -37,42 +35,18 @@ class Layout:
         self.df.index.name = 'date_time'
         self.df.columns = [' '.join([c.strip() for c in col if 'Unnamed' not in c]).lower() for col in self.df.columns]
         self.df.columns = [col.replace(' ', '_').replace('.', '') for col in self.df.columns]
-        self.numeric_variables = self.df.select_dtypes(include=['int', 'float']).columns
+        self.variables = self.df.select_dtypes(include=['int', 'float']).columns
+        self.variable = self.variables[0]
         self.start_date = self.df.index[0]
         self.end_date = self.df.index[-1]
 
     def create_pdf(self, path):
-        with open(os.path.join(os.path.dirname(__file__), 'template.html')) as f:
-            html = Template(f.read())
 
-        css = open(os.path.join(os.path.dirname(__file__), 'style.css')).read()
+        doc = SimpleDocTemplate(path, rightMargin=0, leftMargin=0, topMargin=0, bottomMargin=0,
+                                pagesize=landscape(A4))
 
-        html = html.render(plot=self.plots,
-                           css=css)
+        doc.build([svg2rlg(self.plot)])
 
-        with open(path, "wb") as f:
-            pisa_status = pisa.CreatePDF(html, dest=f)
-
-        return pisa_status.err
-
-
-selected = [
-    'temp_out',
-    'hi_temp',
-    'low_temp',
-    'out_hum',
-    'dew_pt',
-    'wind_speed',
-    'wind_dir',
-    'wind_run',
-    'hi_speed',
-    'hi_dir',
-    'wind_chill',
-    'heat_index',
-    'thw_index',
-    'bar',
-    'rain',
-    'rain_rate',
-    'heat_d-d',
-    'cool_d-dt'
-]
+    def set_variable(self, variable):
+        assert variable in self.variables, '{} not available'.format(variable)
+        self.variable = variable
