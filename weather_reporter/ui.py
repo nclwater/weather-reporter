3f64174b737dfd20fbca5567c1bb21da3315ba9f
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2019-11-11 14:13:49 +0000

    fixed pyinstaller argument and added drag event to show plot

--- weather_reporter/ui.py
+++ weather_reporter/ui.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5 import QtSvg
 from weather_reporter import Layout
 import sys
 
@@ -9,6 +10,31 @@ class App(QMainWindow):
         super().__init__()
         self.show()
         self.activateWindow()
+        self.setAcceptDrops(True)
+        self.path = None
+        self.layout = None
+        self.plot = None
+
+    def dragEnterEvent(self, event):
+        if event.mimeData().hasText():
+            event.accept()
+        else:
+            event.ignore()
+
+    def dropEvent(self, event):
+        for url in event.mimeData().urls():
+            self.path = url.toLocalFile()
+            print(self.path)
+            self.layout = Layout(self.path)
+            self.plot = QtSvg.QSvgWidget()
+            self.plot.load(self.layout.plot.read())
+            self.plot.setGeometry(50, 50, 759, 668)
+            self.setCentralWidget(self.plot)
+            # self.plot.show()
+
+
+            break
+
 
 
 if __name__ == '__main__':
