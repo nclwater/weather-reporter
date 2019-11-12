03e4040240bb9b8d97cd8026c6bd301535d9a61c
Fergus McClean <fergus.mcclean@ncl.ac.uk>
2019-11-12 16:49:27 +0000

    added dropdown to change variable and command line argument for file

--- weather_reporter/ui.py
+++ weather_reporter/ui.py
@@ -1,21 +1,47 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QComboBox, QVBoxLayout, QWidget
 from PyQt5 import QtSvg
 from weather_reporter import Layout
 import sys
 import pandas as pd
 import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f')
+args = parser.parse_args()
 
 
 class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.show()
         self.activateWindow()
         self.setAcceptDrops(True)
         self.path = None
         self.layout = None
-        self.plot = None
+        self.plot = QtSvg.QSvgWidget()
+        self.plot.setMinimumWidth(800)
+        self.plot.setMinimumHeight(500)
+
+        self.mainLayout = QVBoxLayout()
+
+        self.mainWidget = QWidget()
+        self.mainWidget.setLayout(self.mainLayout)
+
+        self.setCentralWidget(self.mainWidget)
+
+        self.variableDropDown = QComboBox()
+
+        self.mainLayout.addWidget(self.variableDropDown)
+        self.mainLayout.addWidget(self.plot)
+
+        self.variableDropDown.activated.connect(self.change_variable)
+
+        if args.f is not None:
+            self.path = args.f
+            self.add_data()
+
+        self.show()
 
     def dragEnterEvent(self, event):
         if event.mimeData().hasText():
@@ -27,17 +53,28 @@ class App(QMainWindow):
         for url in event.mimeData().urls():
             self.path = url.toLocalFile()
             try:
-                self.layout = Layout(self.path)
+                self.add_data()
             except pd.errors.ParserError:
                 msg = QMessageBox()
                 msg.setText('Could not load from {}'.format(os.path.basename(self.path)))
                 msg.exec_()
                 return
-            self.plot = QtSvg.QSvgWidget()
-            self.plot.load(self.layout.plot.read())
-            self.setCentralWidget(self.plot)
-
             break
+
+    def add_data(self):
+        self.layout = Layout(self.path)
+        self.update_plot()
+
+        for var in self.layout.variables:
+            self.variableDropDown.addItem(var)
+
+    def update_plot(self):
+        self.layout.save_plot()
+        self.plot.load(self.layout.plot.read())
+
+    def change_variable(self, variable_idx):
+        self.layout.variable = self.layout.variables[variable_idx]
+        self.update_plot()
 
 
 if __name__ == '__main__':
